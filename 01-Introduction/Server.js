e40203641d1cfa092bd29a0517f081d9d14ad2ba
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-08 13:29:09 +0500

    Get requests for different routes

--- 01-Introduction/Server.js
+++ 01-Introduction/Server.js
@@ -2,6 +2,26 @@
 const express = require('express')
 const app = express()
 
+
+// Get Request  for the Home page
+app.get('/', (req,res) => {
+    res.send("This is the response for the Home Page request: ")
+})
+// Get request for the About Page
+app.get('/about', (req,res) => {
+    res.send("This is the response for the About Page: ")
+})
+// Get request for the Services Page
+app.get('/services', (req,res) => {
+    res.send("This is the response for the Services Page:")
+})
+// Get request for the Contact Page
+app.get('/contact', (req,res) => {
+    res.send("This is the response for the Contact Page:")
+})
+
+
+
 // Server Listening
 app.listen(3000, ()=> {
     console.log('Server is listening on the port 3000')
