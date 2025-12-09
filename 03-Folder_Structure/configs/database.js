0449801430ddffa33bea549a85b6e1663e514a1a
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-09 22:58:42 +0500

    DB connection code in Database file of the config folder

---
+++ 03-Folder_Structure/configs/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+
+const connectDB = () => {
+    mongoose.connect(process.env.DATABASE_URL, )
+.then( () => {
+    console.log("Database connected successfully")
+})
+.catch( (error) => {
+    console.log("Error in DB connection")
+    console.error(error.message)
+    process.exit(1)
+})
+}
+module.exports = connectDB
