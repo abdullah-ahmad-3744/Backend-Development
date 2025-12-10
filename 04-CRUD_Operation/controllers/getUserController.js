e3cb715756c3ee8626eefb97e0821a6134c14161
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-10 23:07:52 +0500

    Get request or read operation from the Postman is working

--- 04-CRUD_Operation/controllers/getUserController.js
+++ 04-CRUD_Operation/controllers/getUserController.js
@@ -1,9 +1,28 @@
+import mongoose from "mongoose"
+import { User } from "../models/userSchema.js"
 
-const getUserController = async () => {
+const getUserController = async (req,res) => {
     try {
-        
+        // Fetching all the users from the DB 
+        const allUsers = await User.find({})
+
+        res.status(200).json(
+            {
+                success : true,
+                data : allUsers,
+                message : 'All users are fetched from DB'
+            }
+        )
     } catch (error) {
-        
+        console.log(error.message)
+        console.error(error.message)
+        res.status(500).json(
+            {
+                success : true,
+                data : 'Error in fetching data: ',
+                message : 'Internal Server Error: '
+            }
+        )
     }
 }
 export default getUserController
