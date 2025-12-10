333140f2a707a184b9fb29ec6db26199ac9ee01b
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-10 12:41:30 +0500

    small changes

--- 04-Creat_User_Operation/server.js
+++ 04-Creat_User_Operation/server.js
@@ -1,8 +1,15 @@
 // Importing express
 import express from 'express'
 const app = express()
+
+
+
 // Importing the Router from the controller file
 import createuserRoute from './routes/createUserRoute.js'
+
+
+
+
 // Importing the DB connection from the configs
 import { connectDB } from '../03-Folder_Structure/configs/database.js'
 
@@ -10,21 +17,25 @@ import { connectDB } from '../03-Folder_Structure/configs/database.js'
 // importing env for using the process object
 import dotenv from 'dotenv'
 dotenv.config()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4000
 
-// middleware for pasing the dara 
+
+
+// // middleware for pasing the dara 
 app.use(express.json())
 
 
 
 // Mapping the API request with the route 
-app.get('/api', createuserRoute)
+app.use('/api', createuserRoute)
 
 
 // Default route for the App
 app.get('/', (req,res) => {
+    console.log("App is working for the defaulr route")
     res.send('<h1>This is Home Page</h1>')
 })
+
 
 // Connecting DB 
 connectDB()
@@ -32,7 +43,6 @@ connectDB()
 
 
 
-// Starting th sever which will listen on a PORT 
 app.listen(PORT, () => {
-    console.log("App is listening on the port 3000")
+    console.log(`App is listening on PORT : ${PORT}`)
 })
