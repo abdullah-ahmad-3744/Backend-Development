af8e0c823c6167d3bd3d746bf47e9701cb93bafe
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-11 12:28:31 +0500

    Backend part done

---
+++ 06-Fullstack/backend/data.js
@@ -0,0 +1,34 @@
+export const users = [
+  {
+    _id: "674a12c1f9e34a9b2a1d1234",
+    name: "Muhammad Abdullah",
+    email: "abdullah@example.com",
+    role: "user",
+    age: 18,
+    createdAt: "2025-01-10T08:45:23.000Z"
+  },
+  {
+    _id: "674a12d4f9e34a9b2a1d5678",
+    name: "Ayesha Khan",
+    email: "ayesha.khan@example.com",
+    role: "admin",
+    age: 22,
+    createdAt: "2025-02-01T12:20:11.000Z"
+  },
+  {
+    _id: "674a12e9f9e34a9b2a1d9101",
+    name: "Ali Raza",
+    email: "ali.raza@example.com",
+    role: "user",
+    age: 24,
+    createdAt: "2025-03-14T15:32:55.000Z"
+  },
+  {
+    _id: "674a130df9e34a9b2a1d1122",
+    name: "Hina Qureshi",
+    email: "hina.q@example.com",
+    role: "moderator",
+    age: 26,
+    createdAt: "2025-04-20T10:05:40.000Z"
+  }
+];
--- 06-Fullstack/backend/server.js
+++ 06-Fullstack/backend/server.js
@@ -1,12 +1,17 @@
 import express from 'express'
 const app = express()
+import { users } from './data.js'
 import dotenv from 'dotenv'
 dotenv.config()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 // Default Route
 app.get('/', (req,res) => {
     res.send('<h1>This is Home Page : </h1>')
+})
+// Seding the users route
+app.get('/users', (req,res) => {
+    res.send(users)
 })
 app.listen(PORT, () => {
     console.log(`App is listening on PORT : ${PORT}`)
