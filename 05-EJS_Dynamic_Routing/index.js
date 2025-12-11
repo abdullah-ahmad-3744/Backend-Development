972f70931965cafea3f6a6dd3fe2b824d7704704
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-11 11:41:02 +0500

    EJS for dynamic content or dynamic templating

--- 05-EJS_Dynamic_Routing/index.js
+++ 05-EJS_Dynamic_Routing/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 const app = express()
+import path from 'path'
 import dotenv from 'dotenv'
 dotenv.config()
 const PORT  = process.env.PORT
@@ -7,12 +8,15 @@ const PORT  = process.env.PORT
 // Midldewares
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-// Setting ejs for view engine
 app.set('view engine', 'ejs')
 
 // default route 
 app.get('/', (req,res) => {
     res.render('index')
+})
+// About page route
+app.get('/about', (req,res) => {
+    res.render('about')
 })
 // Server listenig
 app.listen(PORT, () => {
