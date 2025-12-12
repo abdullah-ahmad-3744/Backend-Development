a4f2889a98c6e4feb010292144d8da98203a9e95
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-12 10:12:58 +0500

    Database connected successfully

--- 07-Blogs_App/server.js
+++ 07-Blogs_App/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 const app = express()
+import { connectDB } from './configs/database.js'
 import dotenv from 'dotenv'
 dotenv.config()
 const PORT = process.env.PORT || 4000
@@ -10,6 +11,9 @@ const PORT = process.env.PORT || 4000
 app.get('/', (req,res) => {
     res.send('<h1> This is Home Page </h1>')
 })
+
+// Conecting DB
+connectDB()
 
 
 app.listen(PORT, () => {
