195d139245c75d0f143bd4a6101c1e5f3b1ef001
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-13 14:29:35 +0500

    Fetching all posts operation tested using postman

--- 07-Blogs_App/controllers/postController.js
+++ 07-Blogs_App/controllers/postController.js
@@ -1,12 +1,11 @@
 import Post from '../models/postModel.js'
 
 // Controller for post Creation 
-
-export const postCreationController = async (req,res) => {
+export const postCreationController = async (req, res) => {
 
     try {
-        let {title, body} = req.body
-        const post  = new Post({
+        let { title, body } = req.body
+        const post = new Post({
             title, body
         })
 
@@ -15,7 +14,7 @@ export const postCreationController = async (req,res) => {
 
         // Sending response 
         res.json({
-            post : savedPost
+            post: savedPost
         })
     } catch (error) {
         console.log(error.message)
@@ -23,8 +22,34 @@ export const postCreationController = async (req,res) => {
 
         res.status(500).json(
             {
-                success : false,
-                message : 'Error in post creation'
+                success: false,
+                message: 'Error in post creation'
+            }
+        )
+    }
+}
+
+// Controller for getting all posts
+
+export const getAllPostsController = async (req, res) => {
+    try {
+        // Getting all posts
+        const posts = await Post.find()
+
+        // Sending response 
+        res.json(
+            {
+                posts
+            }
+        )
+    } catch (error) {
+        console.log(error.message)
+        console.error(error.message)
+
+        res.status(500).json(
+            {
+                success: false,
+                message: 'Error in post creation'
             }
         )
     }
--- 07-Blogs_App/routes/blogRoutes.js
+++ 07-Blogs_App/routes/blogRoutes.js
@@ -3,12 +3,14 @@ const router = express.Router()
 import {dummyRoute } from '../controllers/dummyRouteController.js';
 import { commentController } from '../controllers/commentController.js';
 import { postCreationController } from '../controllers/postController.js';
+import { getAllPostsController } from '../controllers/postController.js';
 
 
 
 router.get('/dummy', dummyRoute)
 router.post('/comments/create', commentController)
 router.post('/posts/create', postCreationController)
+router.get('/posts', getAllPostsController)
 
 
 
